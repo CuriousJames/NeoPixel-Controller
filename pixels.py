14dcf30ca1ad07af96324b0ace8a1cc610df2d68
Curious James <18385102+CuriousJames@users.noreply.github.com>
2021-01-04 21:44:39 +0000

    Update all pixels at once

--- pixels.py
+++ pixels.py
@@ -1,8 +1,9 @@
 import board
 import neopixel
-pixels = neopixel.NeoPixel(board.D18, 600)
+pixels = neopixel.NeoPixel(board.D18, 600, auto_write=False)
 
-pixels[0] = (100, 0, 0)
+pixels[2] = (100, 0, 0)
 pixels[1] = (0, 100, 0)
-pixels[2] = (0, 0, 100)
+pixels[0] = (0, 0, 100)
 pixels[599] = (0, 0, 255)
+pixels.show()
